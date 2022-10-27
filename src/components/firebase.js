5796230ec5b07bc15b9f5ffe404dde36a897ffa8
Jhon Lenon <Jhongamessuper564email@gmail.com>
2022-10-27 11:50:32 -0300

    FIrst

---
+++ src/components/firebase.js
@@ -0,0 +1,19 @@
+/*npm install firebase
+
+  Import the functions you need from the SDKs you need
+  import { initializeApp } from "firebase/app";
+  TODO: Add SDKs for Firebase products that you want to use
+  https://firebase.google.com/docs/web/setup#available-libraries
+
+  Your web app's Firebase configuration
+  const firebaseConfig = {
+  apiKey: "AIzaSyDYOwlXj4-RWXZ1lPiTAXPn4OO39Qg3ayk",
+  authDomain: "aulateste-10c84.firebaseapp.com",
+  projectId: "aulateste-10c84",
+  storageBucket: "aulateste-10c84.appspot.com",
+  messagingSenderId: "696280966119",
+  appId: "1:696280966119:web:c516f45ab3e089b9f99342"
+};
+
+  Initialize Firebase
+  const app = initializeApp(firebaseConfig);*/
